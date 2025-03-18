c04c94d9528754df567b1c1b5b7d9c059972f183
faine1996 <faine@Faine-Omen.>
2025-03-18 19:37:29 +0200

    Faine is frustrated

--- src/copystatic.py
+++ src/copystatic.py
@@ -9,13 +9,19 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
         return  # Skip instead of crashing
 
     if not os.path.exists(dest_dir_path):
+        print(f"DEBUG: Creating destination directory '{dest_dir_path}'...")
         os.mkdir(dest_dir_path)
 
     for filename in os.listdir(source_dir_path):
         from_path = os.path.join(source_dir_path, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        print(f" * {from_path} -> {dest_path}")
+        print(f"DEBUG: Copying {from_path} -> {dest_path}")
+
         if os.path.isfile(from_path):
-            shutil.copy(from_path, dest_path)
+            try:
+                shutil.copy(from_path, dest_path)
+                print(f"SUCCESS: {from_path} copied to {dest_path}")
+            except Exception as e:
+                print(f"ERROR: Could not copy {from_path} -> {dest_path}. Exception: {e}")
         else:
             copy_files_recursive(from_path, dest_path)
